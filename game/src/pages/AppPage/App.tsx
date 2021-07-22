7937dcf5c2b0be754ecedbc7afdb3df448852b18
Stanislav <teplyakov@vtb.ru>
2021-07-22 10:55:53 +0300

    #upd

--- game/src/pages/AppPage/App.tsx
+++ game/src/pages/AppPage/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Game from "../Game/Game";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Game/>
+        </div>
+    );
 }
 
 export default App;
---
+++ game/src/pages/Game/Game.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.css';
+
+function Game() {
+    return (
+        <div className="game-container">
+
+        </div>
+    );
+}
+
+export default Game;
